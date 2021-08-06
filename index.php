420c32545dbcba7c16ba943d2093a68f2e7350a8
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-08-06 12:23:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <header>
          <!-- Fixed navbar -->
          <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand" href="#">Vishal</a>
+            <a class="navbar-brand" href="#">Emax Computer Center</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
